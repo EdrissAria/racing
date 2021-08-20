84ce83231ede43a9396296f3d02bf1451504fc18
Edriss563 <edrissalokozay77@gmail.com>
2021-08-20 05:51:03 -0700

    all done

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
  const car2 = document.getElementById('car2');
  const win1 = document.getElementById('win1');
  const win2 = document.getElementById('win2');
+ const retry = document.getElementById('retry');
 
  var x = 0;
  var y = 0;
@@ -15,11 +16,13 @@
         car2.style.left = y+"px";
         y = y + 30;
      }
-     if(car1.style.left === "1230px"){
+     if(car1.style.left === "1230px" && car2.style.left !== "1230px"){
          win1.style.opacity = '1'
+         retry.style.display = 'block'
      }
-     if(car2.style.left === "1230px"){
+     if(car2.style.left === "1230px" && car1.style.left !== "1230px"){
          win2.style.opacity = '1'
+         retry.style.display = 'block'
      }
      
  })
